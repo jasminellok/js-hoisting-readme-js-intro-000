52dfc13577001585f873d9549567cd9520dfec55
Jasmine Lok <jasminellok@gmail.com>
2020-04-06 03:39:13 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -15,14 +15,11 @@ function crazy() {
   thisIsCrazy()
 }
 
+
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
-    console.log(name);
     var name = "Kristin";
+    console.log(name);
   }
+  sayMy();
 }
